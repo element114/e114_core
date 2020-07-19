c44a45f3c4e49579514c653f1362294d25ec54e5
Andras Mocsary <nobody@reedwolf.com>
2020-07-19 13:08:24 +0200

    ListOptions accepts order and sort as well, alias _start and _end

--- src/lib.rs
+++ src/lib.rs
@@ -17,12 +17,27 @@ use serde::{Deserialize, Serialize};
 #[cfg_attr(feature = "jsonschema", derive(JsonSchema))]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct ListOptions {
+    #[serde(alias = "_start")]
     pub offset: Option<u64>,
+    #[serde(alias = "_end")]
     pub limit: Option<u64>,
+    #[serde(alias = "_order")]
+    pub order: Option<Order>,
+    #[serde(alias = "_sort")]
+    pub sort: Option<String>,
 }
 impl Default for ListOptions {
     #[must_use]
     fn default() -> Self {
-        Self { offset: None, limit: Some(100) }
+        Self { offset: None, limit: Some(100), order: None, sort: None }
     }
+}
+
+#[cfg_attr(feature = "jsonschema", derive(JsonSchema))]
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum Order {
+    #[serde(alias = "ASC", alias = "asc")]
+    Asc,
+    #[serde(alias = "DESC", alias = "desc")]
+    Desc,
 }
