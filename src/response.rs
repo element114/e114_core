074c40327ed6a27830457f24c69345e9a18a1594
Andras Mocsary <nobody@reedwolf.com>
2020-07-23 15:48:42 +0200

    Add custom JsonSchema impl to MessageValue

--- src/response.rs
+++ src/response.rs
@@ -44,7 +44,6 @@ impl Response {
 
 /// @msg is to be used to plain english, user facing feedback messages.
 /// @v: is a json value to be used for application intercommunication purposes.
-#[cfg_attr(feature = "jsonschema", derive(JsonSchema))]
 #[derive(Debug, Clone, Serialize)]
 pub struct MessageValue {
     #[serde(default)]
@@ -53,10 +52,44 @@ pub struct MessageValue {
     v: Option<Value>,
 }
 
+#[cfg(feature = "jsonschema")]
+use schemars::schema::{ SchemaObject, InstanceType};
+#[cfg(feature = "jsonschema")]
+impl JsonSchema for MessageValue {
+    fn schema_name() -> std::string::String { 
+        "MessageValue".to_owned()
+    }
+    fn json_schema(_: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema { 
+        let mut schema = SchemaObject::default();
+        schema.instance_type = Some(InstanceType::Object.into());
+        let mut str_schema = SchemaObject::default();
+        str_schema.instance_type = Some(InstanceType::String.into());
+        // str_schema.object().additional_properties = Some(Box::new(true.into()));
+        str_schema.metadata().default = Some(serde_json::json!(""));
+
+        let mut v_schema = SchemaObject::default();
+        v_schema.instance_type = Some(InstanceType::Object.into());
+        v_schema.object().additional_properties = Some(Box::new(true.into()));
+        v_schema.metadata().default = Some(serde_json::json!({}));
+
+        let obj = schema.object();
+        obj.properties
+            .insert("msg".to_owned(), str_schema.into());
+        obj.properties
+            .insert("v".to_owned(), v_schema.into());
+        schema.metadata().description = Some(r#"@msg is to be used to plain english, user facing feedback messages. @v: is a json value to be used for application intercommunication purposes."#.to_owned());
+        schema.into()
+    }
+}
+
 impl From<Response> for MessageValue {
     #[must_use]
     fn from(resp: Response) -> Self {
-        Self { msg: resp.msg, v: resp.v }
+        if let Some(v) = resp.v {
+            Self::json(resp.msg, v)
+        } else {
+            Self::new(resp.msg)
+        }
     }
 }
 
@@ -74,5 +107,42 @@ impl MessageValue {
     #[must_use]
     pub fn json(msg: String, v: Value) -> Self {
         Self { msg, v: Some(v) }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[allow(unused_imports)]
+    use super::MessageValue;
+    #[cfg(feature = "jsonschema")]
+    use schemars::schema_for;
+
+    #[test]
+    fn test_message_value_schema() {
+        #[cfg(feature = "jsonschema")]
+        {
+            let schema = schema_for!(MessageValue);
+            println!("{}", serde_json::to_string_pretty(&schema).unwrap());
+            assert_eq!(
+                serde_json::json!({
+                    "$schema": "http://json-schema.org/draft-07/schema#",
+                    "description": "@msg is to be used to plain english, user facing feedback messages. @v: is a json value to be used for application intercommunication purposes.",
+                    "properties": {
+                        "msg": {
+                            "default": "",
+                            "type": "string"
+                        },
+                        "v": {
+                            "default": {},
+                            "type": "object",
+                            "additionalProperties": true
+                        },
+                    },
+                    "title": "MessageValue",
+                    "type": "object"
+                }),
+                serde_json::json!(&schema)
+            );
+        }
     }
 }
