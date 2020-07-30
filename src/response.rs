ad747b87160f45ec04db456527db65574a8a4496
Andras Mocsary <nobody@reedwolf.com>
2020-07-30 18:30:31 +0200

    Add ErrorResponse { errors: [MessageValue] }

--- src/response.rs
+++ src/response.rs
@@ -29,62 +29,45 @@ impl From<Result<Value, Response>> for WebResult {
 }
 
 #[derive(Debug, Clone)]
-pub struct Response {
-    pub code: StatusCode,
+pub struct ErrorWithMessage {
     pub msg: String,
     pub v: Option<Value>,
 }
-
+#[derive(Debug, Clone)]
+pub struct Response {
+    pub code: StatusCode,
+    pub errors: Vec<ErrorWithMessage>,
+}
 impl Response {
     #[must_use]
     pub fn with(code: StatusCode, msg: String, v: Option<Value>) -> Self {
-        Self { code, msg, v }
+        Self { code, errors: vec![ErrorWithMessage { msg, v }] }
     }
 }
 
-/// @msg is to be used to plain english, user facing feedback messages.
-/// @v: is a json value to be used for application intercommunication purposes.
+/// @message is to be used to plain english, user facing feedback messages.
+/// @error_type may contain additional information from the server, for example 'Database Error'
+/// @value: is a json value to be used for application intercommunication purposes.
+#[cfg_attr(feature = "jsonschema", derive(JsonSchema))]
 #[derive(Debug, Clone, Serialize)]
 pub struct MessageValue {
     #[serde(default)]
-    msg: String,
-    #[serde(skip_serializing_if = "Option::is_none", default)]
-    v: Option<Value>,
+    message: String,
+    #[serde(default, rename = "errorType")]
+    error_type: Option<String>,
+    #[serde(default)]
+    value: Option<JsObj>,
 }
-
-#[cfg(feature = "jsonschema")]
-use schemars::schema::{ SchemaObject, InstanceType};
-#[cfg(feature = "jsonschema")]
-impl JsonSchema for MessageValue {
-    fn schema_name() -> std::string::String { 
-        "MessageValue".to_owned()
-    }
-    fn json_schema(_: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema { 
-        let mut schema = SchemaObject::default();
-        schema.instance_type = Some(InstanceType::Object.into());
-        let mut str_schema = SchemaObject::default();
-        str_schema.instance_type = Some(InstanceType::String.into());
-        // str_schema.object().additional_properties = Some(Box::new(true.into()));
-        str_schema.metadata().default = Some(serde_json::json!(""));
-
-        let mut v_schema = SchemaObject::default();
-        v_schema.instance_type = Some(InstanceType::Object.into());
-        // v_schema.object().additional_properties = Some(Box::new(true.into()));
-        v_schema.metadata().default = Some(serde_json::json!({}));
-
-        let obj = schema.object();
-        obj.properties
-            .insert("msg".to_owned(), str_schema.into());
-        obj.properties
-            .insert("v".to_owned(), v_schema.into());
-        schema.metadata().description = Some(r#"@msg is to be used to plain english, user facing feedback messages. @v: is a json value to be used for application intercommunication purposes."#.to_owned());
-        schema.into()
-    }
+#[cfg_attr(feature = "jsonschema", derive(JsonSchema))]
+#[derive(Debug, Clone, Serialize)]
+#[cfg_attr(feature = "jsonschema", schemars(transparent))]
+pub struct JsObj {
+    #[serde(flatten)]
+    extra: serde_json::Map<String, Value>,
 }
-
-impl From<Response> for MessageValue {
+impl From<ErrorWithMessage> for MessageValue {
     #[must_use]
-    fn from(resp: Response) -> Self {
+    fn from(resp: ErrorWithMessage) -> Self {
         if let Some(v) = resp.v {
             Self::json(resp.msg, v)
         } else {
@@ -92,21 +75,40 @@ impl From<Response> for MessageValue {
         }
     }
 }
-
 impl MessageValue {
     #[must_use]
-    pub fn new(msg: String) -> Self {
-        Self { msg, v: None }
+    pub fn build_from(resp: Response) -> Vec<MessageValue> {
+        resp.errors.into_iter().map(|err| err.into()).collect()
+    }
+    #[must_use]
+    pub fn new(message: String) -> Self {
+        Self { message, error_type: None, value: None }
     }
 
     #[must_use]
     pub fn str(msg: &str) -> Self {
-        Self { msg: msg.to_owned(), v: None }
+        Self { message: msg.to_owned(), error_type: None, value: None }
     }
 
     #[must_use]
-    pub fn json(msg: String, v: Value) -> Self {
-        Self { msg, v: Some(v) }
+    pub fn json(message: String, v: Value) -> Self {
+        Self {
+            message,
+            error_type: None,
+            value: Some(JsObj { extra: v.as_object().unwrap().to_owned() }),
+        }
+    }
+}
+
+#[cfg_attr(feature = "jsonschema", derive(JsonSchema))]
+#[derive(Debug, Clone, Serialize)]
+pub struct ErrorResponse {
+    pub errors: Vec<MessageValue>,
+}
+impl From<Response> for ErrorResponse {
+    #[must_use]
+    fn from(resp: Response) -> Self {
+        ErrorResponse { errors: MessageValue::build_from(resp) }
     }
 }
 
@@ -126,15 +128,22 @@ mod tests {
             assert_eq!(
                 serde_json::json!({
                     "$schema": "http://json-schema.org/draft-07/schema#",
-                    "description": "@msg is to be used to plain english, user facing feedback messages. @v: is a json value to be used for application intercommunication purposes.",
+                    "description": "@message is to be used to plain english, user facing feedback messages. @error_type may contain additional information from the server, for example 'Database Error' @value: is a json value to be used for application intercommunication purposes.",
                     "properties": {
-                        "msg": {
+                        "errorType": {
+                            "default": null,
+                            "type": [
+                                "string",
+                                "null"
+                            ]
+                        },
+                        "message": {
                             "default": "",
                             "type": "string"
                         },
-                        "v": {
-                            "default": {},
-                            "type": "object",
+                        "value": {
+                            "default": null,
+                            "type": [ "object", "null" ],
                             "additionalProperties": true
                         },
                     },
